5b12ece3bdd699dbf8b0d0eca2578ef771cb9040
emai69063 <55967884+emai69063@users.noreply.github.com>
2020-08-13 14:51:16 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+		System.out.println("Name: Edward Mai");
+		System.out.println("Hobbies: Playing the trumpet, circuitry");
+		System.out.println("College Interests: Higher-tier UCs such as Berkeley or San Diego.");
+		System.out.println("Possible Majors: Comp Sci, Mechanical Engineering");
+		System.out.println("Fun Fact: I've done programming before, and I'm always happy to learn more!");
+
+	}
+}
